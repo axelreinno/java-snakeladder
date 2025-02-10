0c5f8aacbf466d27d9138e0bca3cba520552c499
Axel Reinno Fabiyanto <axel.fabiyanto@nostratech.com>
2025-02-10 11:38:56 +0700

    feat: add snake and ladder

---
+++ Board.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+class Board {
+    private List<Snake> snakes;
+    private List<Ladder> ladders;
+
+    public Board() {
+        this.snakes = new ArrayList<Snake>();
+        this.ladders = new ArrayList<Ladder>();
+    }
+
+    public void addSnake(Snake snake) {
+        snakes.add(snake);
+    }
+
+    public void addLadder(Ladder ladder) {
+        ladders.add(ladder);
+    }
+
+    public int getNextPosition(int position) {
+        for (Snake snake : snakes) {
+            if (position == snake.getHead()) {
+                return snake.getTail();
+            }
+        }
+        for (Ladder ladder : ladders) {
+            if (position == ladder.getBottom()) {
+                return ladder.getTop();
+            }
+        }
+        return position;
+    }
+}
